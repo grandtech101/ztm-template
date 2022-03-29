dab2a470d07c1f7f99d6eb417259bf522fef1fac
Grandtech101 <grandtechsolution101@gmail.com>
2022-03-30 02:23:55 +0800

    Updated homepage with new slogan

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,14 @@
 <template>
-  <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
+  <div class="flex h-screen bg-orange-100 h-[100vh] align-middle text-black text-center">
     <div class="m-auto">
-      <h1 class="text-5xl">Welcome to Zero to Marketing!</h1>
-      <p class="mt-5">Turn your ideas into working web applications.</p>
-    </div>
+      <h1 class="text-5xl font-bold">LunchBreakHUB</h1>
+     <p class="mt-5">Have Lunch with Stranger Online.</p>
+     <div id='right'>
+    <img src="https://images.pexels.com/photos/5490391/pexels-photo-5490391.jpeg?" class='mr-20'  width="300" height="400" style="vertical-align:middle;margin:5px 100px"/>
   </div>
+  </div>
+    </div>
+
+
+    
 </template>
