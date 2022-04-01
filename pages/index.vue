dd6b8778d012a2c1d0404d614489b62e9d1da8e7
Grandtech101 <grandtechsolution101@gmail.com>
2022-04-02 02:22:30 +0800

    updated

--- pages/index.vue
+++ pages/index.vue
@@ -6,13 +6,22 @@
     </div>
 
     <div id="menu-right">
-        <ul class="grid grid-cols-4 text-lg font-roboto text-white pt-5">
+        <ul class="grid grid-cols-4 text-lg font-oswald text-white pt-5">
         <li>About Us</li>
         <li>Menu</li>
         <li>Locations</li>
         <li>Contacts</li>
         </ul>
       </div>
+
+      <div class="grid grid-cols-2 pb-20 pt-10">
+          <img src="hero-burger.png" class="pr-8 mt-10" alt="" />
+          <div>
+          <h1 class="text-8xl text-white text-center font-lilita uppercase">Classic Burger </h1>
+          <h2 class="text-6xl text-white text-center font-lilita uppercase my-5">With Onion </h2>
+          <p class="text-white font-roboto font-light text-center">Our signature burger now comes with onion and cheese .You can customize it to your likings.Freshly made to order,all in a brioche style bun </p>
+      </div>
+       </div>
     </div>
   </div>
 </template>
