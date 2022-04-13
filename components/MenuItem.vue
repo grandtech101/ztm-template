e9c9b353003a81fc61ee733297f09dc54fd7192e
Grandtech101 <grandtechsolution101@gmail.com>
2022-04-13 21:29:42 +0800

    updated

---
+++ components/MenuItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="text-center">
+    <div>
+      <img :src="'/' + item.image" alt="" class="border border-gray p-5" />
+    </div>
+    <h4 class="font-oswald uppercase text-2xl">{{ item.name }}</h4>
+    <p class="font-light text-gray-500 my-2">
+      {{ item.description }}
+    </p>
+    <p class="mt-5 mb-8">
+      <span
+        class="bg-amber-800 text-yellow-400 text-bold text-xl font-oswald font-bold rounded-lg px-5 py-2"
+      >
+        RM{{ item.price.toFixed(2) }}
+      </span>
+    </p>
+    <button class="button font-oswald uppercase border py-2 px-3" @click="addItem">
+      Add to Cart
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['item'],
+  methods: {
+    addItem() {
+      this.$store.commit('addItem', this.item);
+    },
+  },
+};
+</script>
---
+++ pages/menu.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="bg-[url('/menu-page.jpg')]">
+      <div id="menu" class="grid grid-cols-2 py-5">
+        <div id="menu-left">
+          <img src="logo.png" class="w-[150px]" alt="" />
+        </div>
+
+        <div id="menu-right">
+          <ul class="grid grid-cols-4 text-2xl font-oswald text-white pt-5">
+            <li>About Us</li>
+            <li>Our Menu</li>
+            <li>Locations</li>
+            <li>Contacts</li>
+          </ul>
+        </div>
+      </div>
+      <h1 class="text-white font-oswald uppercase text-6xl text-center pt-24 pb-28">
+        Our Menu
+      </h1>
+    </div>
+    <div id="items" class="grid grid-cols-3">
+      <MenuItem v-for="b in items" :key="b.name" :item="b" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [],
+    };
+  },
+  mounted() {
+    this.$axios.get('items.json').then((response) => {
+      this.items = response.data.burgers;
+    });
+  },
+};
+</script>
