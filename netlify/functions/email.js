53966f771e9a865212863241db6d131103b953cb
Grandtech101 <grandtechsolution101@gmail.com>
2022-04-20 05:23:15 +0800

    change

--- netlify/functions/email.js
+++ netlify/functions/email.js
@@ -1,6 +1,6 @@
 const nodemailer = require("nodemailer");
 
-exports.handler = async function (event, context) {
+exports.handler = async (event, context) => {
     const body = JSON.parse(event.body);
     const customerEmail = body.email;
     const orders = body.orders;
@@ -10,45 +10,43 @@ exports.handler = async function (event, context) {
 
     orders.forEach((order) => {
         emailContent = emailContent +
-        `${order.name} - ${order.quantity} pcs - ${order.price*order.quantity }\n`;
+            `${order.name} - ${order.quantity} pcs - ${order.price * order.quantity}\n`;
         total = total + order.price * order.quantity;
-            });
+    });
 
-        emailContent = emailContent + `\n Total Amount: ${total.toFixed(2)}`;
+    emailContent = emailContent + `\n Total Amount: ${total.toFixed(2)}`;
 
-        const email = {
-            from: 'grandtechsolution101@gmail.com',
-            to: customerEmail,
-            subject: "New Order Received",
-            text: emailContent,
+    const email = {
+        from: 'grandtechsolution101@gmail.com',
+        to: customerEmail,
+        subject: "New Order Received",
+        text: emailContent
+    };
+
+    const mailer = nodemailer.createTransport({
+        host: "smtp.sendgrid.net",
+        port: 465,
+        secure: true,
+        auth: {
+            user: "apikey",
+            pass: process.env.sendgridApiKey
+        }
+    });
+
+    try {
+        await mailer.sendMail(email);
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: "Email sent successfully"
+            })
         };
-
-        const mailer = nodemailer.createTransport({
-            host:"smtp.sendgrid.net",
-            port: 465,
-            secure: true,
-            auth:{
-                user: "apikey",
-                pass:process.env.sendgridApiKey
-            }
-        });
-
-        try {
-            await mailer.sendMail(email);
-            return {
-                statusCode: 200,
-                body: JSON.stringify({
-                    message: "Email sent successfully",
-
-                }),
-
-            };
-        }
-        catch (error) {
-            console.log('Error sending email',error);
+    }
+    catch (error) {
+        console.log('Error sending email', error);
 
 
-        }
+    }
 
 
 
