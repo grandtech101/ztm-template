18717c932d29c3276804f937b5ed2b316e26bbd3
Grandtech101 <grandtechsolution101@gmail.com>
2022-04-20 06:32:07 +0800

    change

--- netlify/functions/email.js
+++ netlify/functions/email.js
@@ -1,6 +1,6 @@
 const nodemailer = require("nodemailer");
 
-exports.handler = async function (event, body) {
+exports.handler = async function (event, context) {
     const body = JSON.parse(event.body);
     const customerEmail = body.email;
     const orders = body.orders;
@@ -12,7 +12,7 @@ exports.handler = async function (event, body) {
         emailContent =
         emailContent +
         `${order.name} - ${order.quantity} pcs - ${order.price * order.quantity}\n`;
-        total = total + order.price * order.quantity;
+         total = total + order.price * order.quantity;
     });
 
     emailContent = emailContent + `\n Total Amount: ${total.toFixed(2)}`;
