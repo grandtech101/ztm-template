1bd8142fd0cabad91bf1c6920808e00c20193dba
Grandtech101 <grandtechsolution101@gmail.com>
2022-04-21 13:25:59 +0800

    implemented db functions

--- netlify/db.js
+++ netlify/db.js
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin');
-const { initializeApp,cert}  = require("firebase-admin/app");
-const { getFirestore} = require("firebase-admin/firestore");
+const { initializeApp,cert }  = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
 const serviceAccount = JSON.parse(process.env.firebaseCredential);
 
 exports.handler = async function(event, context) {
@@ -9,7 +9,29 @@ exports.handler = async function(event, context) {
         app = initializeApp({
             credential : cert(serviceAccount),
         });
-
-
     }
-}
+
+    const db =getFirestore(app);
+
+    try{
+        const body = JSON.parse(event.body);
+        const customerEmail = body.email;
+        const orders = body.orders;
+
+        const response = await db.collection('order').add({
+            customerEmail,
+            orders
+        })
+
+        return {
+            statussCode: 200,
+            body: JSON.stringify({message: 'Document added succefully -'+ response.id,
+            }),
+        };
+    
+    }    catch (error){
+        console.log("Error adding document",error);
+    }
+
+    
+};
