00ea6589bf33b82fa858522b7cbd904243087492
Grandtech101 <grandtechsolution101@gmail.com>
2022-04-22 12:43:33 +0800

    updated

--- pages/orders.vue
+++ pages/orders.vue
@@ -19,40 +19,36 @@
       <h1 class="text-white font-oswald uppercase text-6xl text-center pt-24 pb-28">
         Orders
       </h1>
-      <table
-        v-for="order in orders"
-        :key="order.id"
-        class="table-auto w-2/3 mt-20 mx-auto"
-      >
-        <thead>
-          <tr>
-            <td colspan="4">
-              <h1 class="font-oswald uppercase text-2xl pb-10">
-                Order ID - {{ order.id }}
-              </h1>
-            </td>
-          </tr>
-          <tr>
-            <th class="text-left">Name</th>
-            <th class="text-center">Price</th>
-            <th class="text-right">Quantity</th>
-            <th class="text-right">Total</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr
-            v-for="item in order.data.orders"
-            :key="item.name"
-            class="font oswald uppercase text-2xl border-b"
-          >
-            <td class="py-10">{{ item.name }}</td>
-            <td class="text-right">RM {{ item.price.toFixed(2) }}</td>
-            <td class="text-center">{{ item.quantity }}</td>
-            <td class="text-right">RM {{ (item.price * item.quantity).toFixed(2) }}</td>
-          </tr>
-        </tbody>
-      </table>
     </div>
+    <table v-for="order in orders" :key="order.id" class="table-auto w-2/3 mt-20 mx-auto">
+      <thead>
+        <tr>
+          <td colspan="4">
+            <h1 class="font-oswald uppercase text-2xl pb-10">
+              Order ID - {{ order.id }}
+            </h1>
+          </td>
+        </tr>
+        <tr>
+          <th class="text-left">Name</th>
+          <th class="text-center">Price</th>
+          <th class="text-right">Quantity</th>
+          <th class="text-right">Total</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="item in order.data.orders"
+          :key="item.name"
+          class="font oswald uppercase text-2xl border-b"
+        >
+          <td class="py-10">{{ item.name }}</td>
+          <td class="text-right">RM {{ item.price.toFixed(2) }}</td>
+          <td class="text-center">{{ item.quantity }}</td>
+          <td class="text-right">RM {{ (item.price * item.quantity).toFixed(2) }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
