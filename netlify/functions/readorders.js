4b49266c8e3f64242915d8440a43c7d2df1866e8
Grandtech101 <grandtechsolution101@gmail.com>
2022-04-25 03:22:19 +0800

    updated implementation

--- netlify/functions/readorders.js
+++ netlify/functions/readorders.js
@@ -4,6 +4,21 @@ const { getFirestore } = require("firebase-admin/firestore");
 const serviceAccount = JSON.parse(process.env.firebaseCredential);
 
 exports.handler = async function(event, context) {
+    const user = context.clientContext.user;
+
+
+    if (user.email != "grandtechsolution101@gmail.com"){
+        return {
+            statusCode: 401,
+            body: JSON.stringify({
+            message: "You are not allowed to retrieve orders",
+            }),
+        };
+    }
+        
+
+
+
     let app;
     if(admin.apps.length === 0) {
         app = initializeApp({
