d21a3db6d0b4588ba77eafa510f5a6eb9d84015b
Grandtech101 <grandtechsolution101@gmail.com>
2022-04-25 02:57:56 +0800

    implement identity

--- pages/orders.vue
+++ pages/orders.vue
@@ -20,6 +20,24 @@
         Orders
       </h1>
     </div>
+    <div v-if="user === null" id="login" class="text-center mt-20">
+      <p>This page is limited to authorize users only.</p>
+      <button
+        class="font-oswald uppercase bg-red-500 text-white py-3 px-8 mt-5"
+        @click="login"
+      >
+        Login
+      </button>
+    </div>
+    <div v-if="user" id="logout" class="text-center mt-20">
+      <button
+        class="font-oswald uppercase bg-red-500 text-white py-3 px-8"
+        @click="logout"
+      >
+        Logout
+      </button>
+    </div>
+
     <table v-for="order in orders" :key="order.id" class="table-auto w-2/3 mt-20 mx-auto">
       <thead>
         <tr>
@@ -59,11 +77,43 @@ export default {
       orders: [],
     };
   },
+  head() {
+    return {
+      script: [
+        {
+          src: 'https://identity.netlify.com/v1/netlify-identity-widget.js',
+        },
+      ],
+    };
+  },
 
   mounted() {
-    this.$axios.get('/.netlify/functions/readorders').then((response) => {
-      this.orders = response.data;
-    });
+    this.user = window.netlifyIdentity.currentUser();
+    if (this.user) {
+      this.readOrders();
+    }
+  },
+
+  methods: {
+    readOrders() {
+      this.$axios.get('/.netlify/functions/readorders').then((response) => {
+        this.orders = response.data;
+      });
+    },
+
+    login() {
+      window.netlifyIdentity.open();
+      window.netlify.Identity.on('login', (user) => {
+        this.user = user;
+        this.readOrders();
+      });
+    },
+
+    logout() {
+      window.netlifyIdentity.logout();
+      this.user = null;
+      this.orders = [];
+    },
   },
 };
 </script>
